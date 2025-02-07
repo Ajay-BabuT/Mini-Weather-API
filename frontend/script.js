99ead5cbf1772f68c214a1caac444ee8d051546b
Ajay Babu T <ajaybabu9895@gmail.com>
2025-02-07 21:26:22 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -13,9 +13,9 @@ document.getElementById("getWeather").addEventListener("click", async () => {
             document.getElementById("weather-result").innerHTML = `
                 <h3>${data.city}</h3>
                 <p>Temperature: ${data.temperature}°C</p>
-                <p>Feels Like: ${data.feels_like}°C</p>
+                <p>Feels Like: ${data.average}°C</p>
                 <p>Humidity: ${data.humidity}%</p>
-                <p>Description: ${data.description}</p>
+                <p>Description: ${data.mode}</p>
             `;
         }
     } catch (error) {
